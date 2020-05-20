5d3b382772321e02eb803e3940cd77fccce7dcfc
Juan <juanpabloguleal@gmail.com>
2020-05-20 10:14:24 -0500

    CRUD operations for users added

---
+++ Bulky Book/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bulky_Book.DataAccess;
+using Bulky_Book.DataAccess.Repository;
+using Bulky_Book.DataAccess.Repository.IRepository;
+using Bulky_Book.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky_Book.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _db.ApplicationUsers.Include(x => x.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in users)
+            {
+                var roleId = userRole.FirstOrDefault(x => x.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(x => x.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+            return Json(new { data = users });
+        }
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            var user = _db.ApplicationUsers.Where(x => x.Id == id).FirstOrDefault();
+            if (user == null)
+            {
+                return Json(new { success = false, message = "Error while unlocking/locking" });
+            }
+            else if (user.LockoutEnd != null && user.LockoutEnd > DateTime.Now)
+            {
+                user.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                user.LockoutEnd = DateTime.Now.AddDays(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation successful" });
+        }
+    }
+}
---
+++ Bulky Book/wwwroot/js/user.js
@@ -0,0 +1,94 @@
+﻿var dataTable;
+$(document).ready(function () {
+    LoadDataTable();
+});
+
+function LoadDataTable() {
+    dataTable = $('#dataTable').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            {
+                "data": "name",
+                "width": "15%"
+            }, {
+                "data": "email",
+                "width": "15%"
+            }, {
+                "data": "phoneNumber",
+                "width": "15%"
+            }, {
+                "data": "company.name",
+                "width": "15%"
+            }, {
+                "data": "role",
+                "width": "15%"
+            },
+            {
+                "data": { id: "id", lockoutEnd: "lockoutEnd"},
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        return `
+                        <div class="text-center">
+                            <a onclick="LockUnlock('${data.id}')" class="btn btn-sm btn-danger"><i class="fas fa-lock-open"></i>Unlock</a>
+                        </div>`;
+                    }
+                    else {
+                        return `
+                        <div class="text-center">
+                            <a onclick="LockUnlock('${data.id}')" class="btn btn-sm btn-success"><i class="fas fa-lock"></i>Lock</a>
+                        </div>`;
+                    }
+                    
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+function LockUnlock(id) {
+    $.ajax({
+        url: "/Admin/User/LockUnlock",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(id),
+        success: function (data) {
+            if (data.success == true) {
+                swal(data.message, {
+                    icon: "success",
+                });
+                dataTable.ajax.reload();
+            }
+            else {
+                swal(data.message);
+            }
+        }
+    });
+
+    //No library/plugins version
+    /*
+    var remove = confirm("You're about to remove the User, this action can't be reverted, are you sure?");
+    var params = { id: id };
+    console.log(params);
+    if (remove) {
+        $.ajax({
+            url: "/Admin/User/Remove/" + id,
+            method: "post",
+            dataType: "json",
+            data: params,
+            success: function (data) {
+                if (data.result == true) {
+                    dataTable.ajax.reload();
+                }
+                else {
+
+                }
+            }
+        });
+    }
+    */
+}
